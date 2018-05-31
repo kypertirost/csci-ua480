1300105b69be1d76ccb1b95c43c6893794bf7e91
kypertirost <liumiao1018@me.com>
2018-05-31 18:46:56 -0400

    ml6468 Assignment 01

---
+++ Assets/Assignments/Assignment_01/A01_ml6468/Scripts/Straight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ml6468;
+
+public class Straight : MonoBehaviour {
+
+    public float speed = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.up * Time.deltaTime * speed);
+	}
+}
---
+++ Assets/Assignments/Assignment_01/A01_ml6468/Scripts/StriaghtCenter.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ml6468;
+
+public class StriaghtCenter : MonoBehaviour {
+
+    public float distance = 5f;
+    public float angle = 10f;
+
+    Transform center;
+    Transform ballChild;
+
+    List<Vector3> posList = new List<Vector3>();
+    void Start()
+    {
+        center = transform;
+        ballChild = GameObject.CreatePrimitive(PrimitiveType.Sphere).transform;
+        center.name = "center";
+        ballChild.name = "ball";
+
+        ballChild.position = center.position + Vector3.Normalize(Vector3.up) * distance;
+
+    }
+
+
+
+    void Update()
+    {
+        Quaternion rotate = Quaternion.AngleAxis(angle, Vector3.right);
+        Vector3 directionVector = Vector3.Normalize(ballChild.position - center.position);
+        Vector3 inwardVector = rotate * directionVector * distance;
+        ballChild.position = center.position + inwardVector;
+
+        posList.Add(ballChild.position);
+
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        for (int i = 0; i < posList.Count - 1; i++)
+        {
+            Vector3 from = posList[i];
+            Vector3 to = posList[i + 1];
+            Gizmos.DrawLine(from, to);
+        }
+    }
+}
