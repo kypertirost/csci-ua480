0ceb6d6bd7967d3656e353adccb9b2dce915f106
kypertirost <liumiao1018@me.com>
2018-07-01 19:59:52 -0400

    complete first & second part of the homework

---
+++ Assets/Assignments/Assignment_04/A04_ml6468/Scripts/MoveHandler.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace ml6468.A04{
+    public class MoveHandler : MonoBehaviour
+    {
+        /*
+         * 
+         * 
+         */
+        private float currentSpeed;
+        private bool isMoving;
+        private bool toAccelarating;
+        float speed;
+        // Use this for initialization
+        void Start()
+        {
+            speed = 1.8f;
+            isMoving = false;
+            currentSpeed = 0.0f;
+            toAccelarating = true;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButton(0)) {
+                //holding the mouse, accelerate to full speed
+                isMoving = true;
+
+                currentSpeed = Mathf.Lerp(currentSpeed, speed, Time.deltaTime); // lerp to full speed
+                Vector3 forward = Camera.main.transform.forward;
+                forward.y = 0;
+                transform.Translate(forward * currentSpeed * Time.deltaTime);
+                
+            } else if (isMoving) {
+                //decelerate
+                currentSpeed = Mathf.Lerp(currentSpeed, 0, Time.deltaTime); // lerp to zero speed
+                Vector3 forward = Camera.main.transform.forward;
+                forward.y = 0;
+                transform.Translate(forward * currentSpeed * Time.deltaTime);
+                if (currentSpeed - 0.0f < 0.001f) {
+                    isMoving = false;
+                }
+            }
+
+
+        }
+
+  
+    }
+}
+
---
+++ Assets/Assignments/Assignment_04/A04_ml6468/Scripts/Teleport.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace ml6468.A04{
+    public class Teleport : MonoBehaviour
+    {
+        /*
+         * This script is for part 1. When user select the white circle, he can 
+         * directly teleport to that position.
+         */
+        public GameObject player;
+        float playerHeight;
+
+        void Start()
+        {
+            playerHeight = player.transform.position.y;
+        }
+
+        public void TeleportToCircle()
+        {
+            //move to postion, then rise the height
+            player.transform.position = transform.position;
+            player.transform.position = new Vector3(player.transform.position.x, playerHeight, player.transform.position.z);
+        }
+
+    } 
+}
+
