9e7b4a52f5452e272411923c9d19d3b44f3f5561
kypertirost <liumiao1018@me.com>
2018-07-03 18:58:51 -0400

    part 3 version1 complete

---
+++ Assets/Assignments/Assignment_04/A04_ml6468/Scripts/Raycast.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace ml6468.A04{
+    public class Raycast : MonoBehaviour
+    {
+        /*
+         *  
+         * 
+         * 
+         */
+        public float distance = 10.0f;
+        RaycastHit hitInfo;
+        private float time;
+        private float moveTime = 2.0f;
+        private IEnumerator coroutine;
+
+        private void Start()
+        {
+            
+        }
+        void Update()
+        {
+            
+
+            if (Physics.Raycast(transform.position, transform.forward, out hitInfo, distance))
+            {
+                Debug.Log("start coroutine");
+                coroutine = RaycastGaze();
+                StartCoroutine(coroutine);
+            } 
+
+        }
+
+        private IEnumerator RaycastGaze() {
+            time = 0.0f;
+            Debug.Log("hit");
+            while (time < moveTime) {
+                time += Time.deltaTime;
+                yield return null;
+            }
+            Teleport();
+        }
+
+        private void Teleport() {
+            Vector3 teleport = hitInfo.point;
+            teleport.y = transform.parent.position.y;
+            transform.parent.transform.position = teleport;
+            time = 0;
+            StopCoroutine(coroutine);
+        }
+
+    }
+}
+
