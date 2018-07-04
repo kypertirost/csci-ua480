b955a8c04c1a4f9d95cf6517eacc053eb7d7e45b
kypertirost <liumiao1018@me.com>
2018-07-04 13:52:43 -0400

    start part 4

--- Assets/Assignments/Assignment_04/A04_ml6468/Scripts/MoveHandler.cs
+++ Assets/Assignments/Assignment_04/A04_ml6468/Scripts/MoveHandler.cs
@@ -5,43 +5,28 @@ namespace ml6468.A04{
     public class MoveHandler : MonoBehaviour
     {
         /*
-         * 
-         * 
+         * This script is for part 2. User can push the button to accelerate
+         * to full speed, and release the button to deaccelerate to the rest.
          */
-        public static MoveHandler Player;
         private float currentSpeed;
         private bool isMoving;
-
-        float speed;
-
-
-        private void Awake()
-        {
-            // prevent NullPointer, and set Player to current object
-            if (Player == null) {
-                Player = this;
-            } else if (Player != this) {
-                Destroy(this);
-            }
-            speed = 1.8f;
-            isMoving = false;
-            currentSpeed = 0.0f;
-        }
-
-
+        private float speed;
         // Update is called once per frame
+        private void Start()
+        {
+            speed = 2.0f;
+            isMoving = false;
+            currentSpeed = 0;
+        }
         void Update()
         {
-            MoveControl();
-        }
-
-        public void MoveControl(){
             if (Input.GetMouseButton(0))
             {
                 //holding the mouse, accelerate to full speed
+                Debug.Log("click");
                 isMoving = true;
-
                 currentSpeed = Mathf.Lerp(currentSpeed, speed, Time.deltaTime); // lerp to full speed
+                Debug.Log(currentSpeed);
                 Vector3 forward = Camera.main.transform.forward;
                 forward.y = 0;
                 transform.Translate(forward * currentSpeed * Time.deltaTime);
@@ -49,16 +34,21 @@ namespace ml6468.A04{
             }
             else if (isMoving)
             {
-                //decelerate
+                //deaccelerate
                 currentSpeed = Mathf.Lerp(currentSpeed, 0, Time.deltaTime); // lerp to zero speed
                 Vector3 forward = Camera.main.transform.forward;
                 forward.y = 0;
                 transform.Translate(forward * currentSpeed * Time.deltaTime);
                 if (currentSpeed - 0.0f < 0.001f)
                 {
+                    // prevent huge calculation, stop execute if speed is undetectable
                     isMoving = false;
                 }
             }
+        }
+
+        public void MoveControl(){
+            
         }
   
     }
--- Assets/Assignments/Assignment_04/A04_ml6468/Scripts/Raycast.cs
+++ Assets/Assignments/Assignment_04/A04_ml6468/Scripts/Raycast.cs
@@ -5,20 +5,19 @@ namespace ml6468.A04{
     public class Raycast : MonoBehaviour
     {
         /*
-         *  
+         *  This is for assignment part 3.
          * 
+         *  When an user gaze within 10 meters for 2 seconds, this script is enable,
+         *  and the user can teleport to the point. 
+         * 
+         *  If a player gaze elsewhere, the coroutine will stop and let user can
+         *  gradually move if he or she push the button down.
          * 
          */
         public float distance = 10.0f;
-        RaycastHit hitInfo;
-        private float time;
-        private float moveTime = 3.0f;
+        private RaycastHit hitInfo;
         private IEnumerator coroutine;
 
-        private void Start()
-        {
-            
-        }
         void Update()
         {
             coroutine = RaycastGaze();
@@ -29,15 +28,17 @@ namespace ml6468.A04{
             while (true) {
                 if (Physics.Raycast(transform.position, transform.forward, out hitInfo, distance))
                 {
-                    Debug.Log("start coroutine");
                     yield return new WaitForSeconds(2);
                     Teleport();
+                } else {
+                    break;
                 }
                 yield return null;
             }
         }
 
         private void Teleport() {
+            // teleport to the location and stop coroutine after 
             Vector3 teleport = hitInfo.point;
             teleport.y = transform.parent.position.y;
             transform.parent.transform.position = teleport;
