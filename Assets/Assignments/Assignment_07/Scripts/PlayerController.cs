a8a9128c1815394660069d00bbec99f5fb0b018a
kypertirost <liumiao1018@me.com>
2018-07-22 13:51:43 -0400

    fixed bugs, need to improve visual effects

--- Assets/Assignments/Assignment_07/Scripts/PlayerController.cs
+++ Assets/Assignments/Assignment_07/Scripts/PlayerController.cs
@@ -3,55 +3,75 @@ using UnityEngine.Networking;
 
 namespace ml6468 {
     public class PlayerController : NetworkBehaviour {
-    public GameObject bulletPrefab;
-    public Transform bulletSpawn;
+        
+        public GameObject bulletPrefab;
+        public Transform bulletSpawn;
 
-    private void Start()
-    {
-        Camera.main.transform.parent = transform;
-    }
-
-    void Update()
-    {
-        if (!isLocalPlayer)
+        private void Start()
         {
-            return;
-        }
+            GameObject empty = new GameObject();
+            transform.parent = empty.transform;
+            Camera.main.transform.parent = transform.parent;
             transform.forward = Camera.main.transform.forward;
-        //var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        //var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
-
-        //transform.Rotate(0, x, 0);
-        //transform.Translate(0, 0, z);
-
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            CmdFire();
         }
+
+        void Update()
+        {
+            if (!isLocalPlayer)
+            {
+                return;
+            }
+            //bind camera and player prefab
+            var transformPosition = transform.position;
+            transformPosition.y = 0.39f;
+            Camera.main.transform.position = transformPosition;
+            transform.rotation = Camera.main.transform.rotation;
+
+            //identify move or shoot
+            if (Input.GetMouseButtonDown(0)) {
+                transform.parent.Translate(Vector3.zero);
+                CmdFire();
+            } else if (Input.GetMouseButtonUp(0)) {
+                ExitMove();
+            } else if (Input.GetMouseButton(0)) {
+                PushMove();
+            } 
+        }
+
+        // This [Command] code is called on the Client …
+        // … but it is run on the Server!
+        [Command]
+        void CmdFire()
+        {
+            var bullet = (GameObject)Instantiate(
+                bulletPrefab,
+                bulletSpawn.position,
+                bulletSpawn.rotation
+            );
+
+            bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+            NetworkServer.Spawn(bullet);
+
+            Destroy(bullet, 2.0f);
+        }
+
+        public override void OnStartLocalPlayer()
+        {
+            GetComponent<MeshRenderer>().material.color = Color.blue;
+        }
+
+
+
+        private void PushMove() {
+            var move = Camera.main.transform.forward * Time.deltaTime * 4.0f;
+            move.y = 0;
+            transform.parent.Translate(move);
+        }
+
+        private void ExitMove() {
+            transform.parent.Translate(Vector3.zero);
+        }
+
     }
-
-    // This [Command] code is called on the Client …
-    // … but it is run on the Server!
-    [Command]
-    void CmdFire()
-    {
-        var bullet = (GameObject)Instantiate(
-            bulletPrefab,
-            bulletSpawn.position,
-            bulletSpawn.rotation
-        );
-
-        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
-
-        NetworkServer.Spawn(bullet);
-
-        Destroy(bullet, 2.0f);
-    }
-
-    public override void OnStartLocalPlayer()
-    {
-        GetComponent<MeshRenderer>().material.color = Color.blue;
-    }
-
-
-}
 }
